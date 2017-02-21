7661aec0855fe7004a140282966cc7a90ff6edc7
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-21 19:57:50 +1100

    Created class Library and basic getBookList to pass function

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nataliadjohari on 21/02/2017.
+ */
+public class Library {
+
+    public List getBookList() {
+        return new ArrayList();
+    }
+}
