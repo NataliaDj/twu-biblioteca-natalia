82a8ba2d44060d7c88c9fb8ddaecf3af3a7d1ebb
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-21 21:21:40 +1100

    Refactored tests and added test to test whether the right book was added

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,30 +1,44 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 
 /**
  * Created by nataliadjohari on 21/02/2017.
  */
 public class LibraryTest {
+    Library lib;
+
+    @Before
+    public void setup() {
+        lib = new Library();
+    }
 
     @Test
     public void getBookListReturnsAListTest() {
-        Library lib = new Library();
         assertThat(lib.getBookList(), instanceOf(java.util.List.class));
     }
 
     @Test
     public void addBookAddsTheBookToBookListTest() {
-        Library lib = new Library();
         lib.addBook(new Book());
         assertThat(lib.getBookList().size(), is(1));
     }
 
     //test library contains book that was added
+    @Test
+    public void addBookAddsSameBookToBookListTest() {
+        Book book = new Book("Call of The Wild");
+        lib.addBook(book);
+        assertThat(lib.getBookList(), hasItem(book));
+    }
 
     //test add book does not change other books in library
 }
