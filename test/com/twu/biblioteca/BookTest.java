5b7c4d72a5d8aea49b9905aa32f33399d82ee628
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-21 20:28:54 +1100

    Changed test name to include word "test" and added a new test

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -2,14 +2,25 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by nataliadjohari on 21/02/2017.
  */
 public class BookTest {
 
     @Test
-    public void emptyBookReturnEmptyStringTitle() {
+    public void emptyBookReturnEmptyStringTitleTest() {
         Book book = new Book();
         assertThat(book.getTitle(), is(""));
     }
+
+    @Test
+    public void getTitleReturnsBookTitleTest() {
+        String title = "Harry Potter and The Chamber of Secrets";
+        Book book = new Book(title);
+        assertThat(book.getTitle(), is(title));
+    }
+
 }
