3504fd62c26d2d5fe0e0412fb75acef59935bb5c
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-21 19:34:50 +1100

    cleaned up imports and added comment

--- test/com/twu/biblioteca/WelcomeTest.java
+++ test/com/twu/biblioteca/WelcomeTest.java
@@ -3,8 +3,6 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.annotation.PreDestroy;
-
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,6 +10,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by nataliadjohari on 21/02/2017.
  */
 public class WelcomeTest {
+    /**
+     * Welcome Message
+     *
+     * As a customer,
+     * I would like to see a welcome message when I start the application,
+     * so that
+     * I feel welcome and know that Biblioteca is available.
+     */
 
     BibliotecaApp app;
 
