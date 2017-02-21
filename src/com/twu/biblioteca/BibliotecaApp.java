0f4d34c2a0fb8c85572149a40de2d1fd08fd2b0e
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-21 22:59:19 +1100

    create functions to pass the biblioteac app tests

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,18 +1,36 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
+    Library lib;
 
     public static void main(String[] args) {
         BibliotecaApp app = new BibliotecaApp();
         System.out.println(app.getWelcomeMessage());
 
-        Library lib = new Library();
-        for (Book book: lib.getBookList()) {
-            System.out.println(book.getDetails());
-        }
+
     }
 
     public String getWelcomeMessage() {
         return "Welcome to Biblioteca\n";
+    }
+
+    public List<String> getMenuOptions() {
+        List<String> options = new ArrayList<String>();
+        options.add("List Books");
+        return options;
+    }
+
+    public String getSelectedMenuOptionResult(String option) {
+        if (option == "List Books") {
+            String result = "";
+            for (Book book: lib.getBookList()) {
+                result += book.getDetails() + "\n";
+            }
+            return result;
+        }
+        return null;
     }
 }
