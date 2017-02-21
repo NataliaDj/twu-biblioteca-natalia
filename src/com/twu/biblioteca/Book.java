aab1ac28d64f702674b7044f743cd2eedb4ca818
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-21 20:32:22 +1100

    Created book constructors and modified get title to pass BookTest

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,8 +4,17 @@ package com.twu.biblioteca;
  * Created by nataliadjohari on 21/02/2017.
  */
 public class Book {
+    private String title;
+
+    public Book() {
+        this.title = "";
+    }
+
+    public Book(String title) {
+        this.title = title;
+    }
 
     public String getTitle() {
-        return "";
+        return title;
     }
 }
