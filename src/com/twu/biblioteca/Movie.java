27856775fae3636cd5779da73d1f3698f4d30a0c
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-26 16:13:38 +1100

    Added Movie to pass MovieTest

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by nataliadjohari on 26/02/2017.
+ */
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
