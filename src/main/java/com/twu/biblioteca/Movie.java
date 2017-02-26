a2cb27abd09c0136e0ade68b6db3ba0a09ddc4d0
Natalia Djohari <nataliadjohari@Natalias-MBP.home.gateway>
2017-02-26 16:54:02 +1100

    Modified Movie to pass test

--- src/main/java/com/twu/biblioteca/Movie.java
+++ src/main/java/com/twu/biblioteca/Movie.java
@@ -5,12 +5,18 @@ package main.java.com.twu.biblioteca;
  */
 public class Movie {
     private String title;
+    private String director;
 
-    public Movie(String title) {
+    public Movie(String title, String director) {
         this.title = title;
+        this.director = director;
     }
 
     public String getTitle() {
         return this.title;
+    }
+
+    public String getDirector() {
+        return this.director;
     }
 }
